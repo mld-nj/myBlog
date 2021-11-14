d5adb3cb7de5bde32a412df3bcced80d5a5e5ea7
lili kai <1761311608@qq.com>
2021-11-14 14:43:15 +0800

    complete timeAxis component

--- src/components/TimeAxis/TimeAxis.tsx
+++ src/components/TimeAxis/TimeAxis.tsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getCardDetails } from "../../api/cardMes";
 import "./TimeAxis.scss";
-const PasNodeItem = () => {
+const PasNodeItem = (props: any) => {
   return (
     <div className="nodeItemContainer">
       <div className="circle"></div>
-      <div className="time">2021 / 11 / 01</div>
+      <div className="icon"></div>
+      <div className="time">{props.date.slice(0, 10)}</div>
       <div className="divide"></div>
-      <Link className="title" to={`/passage`}>
-        hhh
+      <Link className="title" to={`/passage/${props.id}`}>
+        {props.title}
       </Link>
     </div>
   );
 };
 const TimeAxis = () => {
+  const [cardList, setCardList] = useState([]);
+  useEffect(() => {
+    getCardDetails().then((res) => {
+      setCardList(JSON.parse(res.data));
+    });
+  }, []);
+  useEffect(() => {
+    console.log(cardList);
+  }, [cardList]);
   return (
     <div className="timeAxisContainer">
       <div className="pasNodeContainer">
-        <PasNodeItem></PasNodeItem>
-        <PasNodeItem></PasNodeItem>
-        <PasNodeItem></PasNodeItem>
+        {cardList.map((value: any) => {
+          return (
+            <PasNodeItem
+              date={value.date}
+              title={value.name}
+              key={value.id}
+              id={value.id}
+            ></PasNodeItem>
+          );
+        })}
       </div>
     </div>
   );
